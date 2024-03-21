5cc5c1c5597446e05f18da2a16b4b395301ad0b8
Work <yakushevso@ya.ru>
2024-03-21 08:50:38 +0300

    Stage 2/5: Equalizing chances

--- Rock-Paper-Scissors (Java)/task/src/rockpaperscissors/Main.java
+++ Rock-Paper-Scissors (Java)/task/src/rockpaperscissors/Main.java
@@ -1,19 +1,31 @@
 package rockpaperscissors;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            String users = sc.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+            String users = scanner.nextLine();
 
-            String computer = switch (users) {
-                case "paper" -> "scissors";
-                case "scissors" -> "rock";
+            String computer = switch (new Random().nextInt(3)) {
+                case 0 -> "scissors";
+                case 1 -> "rock";
                 default -> "paper";
             };
 
-            System.out.println("Sorry, but the computer chose " + computer);
+            checkWin(users, computer);
+        }
+    }
+
+    private static void checkWin(String users, String computer) {
+        if (users.equals(computer)) {
+            System.out.printf("There is a draw (%s)", computer);
+        } else if (users.equals("paper") && computer.equals("scissors") ||
+                users.equals("scissors") && computer.equals("rock") ||
+                users.equals("rock") && computer.equals("paper")) {
+            System.out.printf("Sorry, but the computer chose %s", computer);
+        } else {
+            System.out.printf("Well done. The computer chose %s and failed", computer);
         }
     }
 }
--- Rock-Paper-Scissors (Java)/task/test/Tests.java
+++ Rock-Paper-Scissors (Java)/task/test/Tests.java
@@ -3,40 +3,120 @@ import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.testcase.TestCase;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.List;
 
 public class Tests extends StageTest<String> {
-    String[] cases = new String[]{"rock", "paper", "scissors"};
+    String[] cases = new String[]{"rock",
+            "paper",
+            "scissors"};
+    int loses = 0;
+    int wins = 0;
+    int draws = 0;
 
-    public List<TestCase<String>> generate() {
-        List<TestCase<String>> tests = new ArrayList<>();
-        for (String input : cases) {
-            TestCase<String> testCase = new TestCase<>();
-            testCase.setAttach(input + "\n" + getWin(input));
-            testCase.setInput(input);
-            tests.add(testCase);
-        }
-        return tests;
+    CheckResult checkRandom(String reply, String attach) {
+        CheckResult wrongRandomize = CheckResult.wrong(String.format(
+                "The results of the games: %s wins, %s draws and %s loses\n" +
+                        "Looks like you don't use the random module to choose a random option!\n" +
+                        "The number of wins, draws and loses should be approximately the same.\n" +
+                        "Make sure you output the results of the games the same way as in the examples!\n" +
+                        "If you are sure that you use random module try to rerun the tests!\n", wins, draws, loses));
+        if (loses < 30)
+            return wrongRandomize;
+        if (draws < 30)
+            return wrongRandomize;
+        if (wins < 30)
+            return wrongRandomize;
+        return CheckResult.correct();
     }
 
     public CheckResult check(String reply, String attach) {
-        String[] atts = attach.split("\n");
-        String correctOutput = String.format("Sorry, but the computer chose %s", atts[1].strip());
-        if (!correctOutput.toLowerCase().replaceAll("[^a-z0-9]+", "").equals(
-                reply.strip().toLowerCase().replaceAll("[^a-z0-9]+", ""))) {
-            return CheckResult.wrong(String.format("Your answer on \"%s\" was \"%s\". " +
-                    "This is a wrong output. The correct output is \"%s\"", atts[0], reply.strip(), correctOutput));
+        CheckResult wrongResult = CheckResult.wrong(String.format(
+                "Seems like your answer (\"%s\") is either inconsistent " +
+                        "with the rock-paper-scissors rules or the string is formatted incorrectly.  " +
+                        "Check punctuation, spelling, and capitalization of your output. " +
+                        "Also, make sure you are following the rules of the game.", reply.strip()));
+        Dictionary<String, String> hits = new Hashtable<>();
+        hits.put("rock", "scissors");
+        hits.put("scissors", "paper");
+        hits.put("paper", "rock");
+
+        String computerOption = "not found";
+
+
+
+        if ((reply.toLowerCase().contains("scissors") && (reply.toLowerCase().contains("paper") || reply.toLowerCase().contains("rock"))) ||
+                (reply.toLowerCase().contains("paper") && reply.toLowerCase().contains("rock"))) {
+            return CheckResult.wrong("Seems like your answer (\"" + reply.strip() + "\") contains more than one of possible" +
+                    " options ['scissors', 'paper', 'rock']");
         }
+
+        if (reply.toLowerCase().contains("scissors"))
+            computerOption = "scissors";
+        else if (reply.toLowerCase().contains("paper"))
+            computerOption = "paper";
+        else if (reply.toLowerCase().contains("rock"))
+            computerOption = "rock";
+
+        if (computerOption.contains("not found"))
+            return wrongResult;
+
+        if ((reply.toLowerCase().contains("well done") && (reply.toLowerCase().contains("draw") || reply.toLowerCase().contains("sorry"))) ||
+                (reply.toLowerCase().contains("draw") && reply.toLowerCase().contains("sorry"))) {
+            return CheckResult.wrong("Seems like your answer (\"" + reply.strip() + "\") contains more than one of possible" +
+                    " results");
+        }
+
+        String result;
+        String feedback;
+        if (hits.get(attach).equals(computerOption)) {
+            result = "well done";
+            feedback = String.format("Well done. The computer chose %s and failed", hits.get(attach));
+        } else if (attach.contains(computerOption)) {
+            result = "draw";
+            feedback = String.format("There is a draw (%s)", hits.get(attach));
+        } else {
+            result = "sorry";
+            feedback = String.format("Sorry, but the computer chose %s", hits.get(attach));
+        }
+
+        if (!reply.toLowerCase().contains(result))
+            return CheckResult.wrong(String.format("Incorrect result found in your answer (\"%s\") when player chose " +
+                    "\"%s\" and computer chose \"%s\".\nExpected: \"%s\"", reply.strip(), attach, hits.get(attach),
+                    feedback));
+
+        if (reply.toLowerCase().contains("sorry"))
+            loses += 1;
+        else if (reply.toLowerCase().contains("draw"))
+            draws += 1;
+        else if (reply.toLowerCase().contains("well done"))
+            wins += 1;
+        else
+            return wrongResult;
         return CheckResult.correct();
 
     }
 
-    String getWin(String input) {
-        for (int i = 0; i < cases.length; i++)
-            if (input.equals(cases[i])) {
-                if (i + 1 < cases.length) return cases[i + 1];
-                else return cases[0];
-            }
-        return "";
+    public List<TestCase<String>> generate() {
+        List<String> inputs = new ArrayList<>();
+        for (String option : cases) {
+            for (int i = 0; i < 50; i++)
+                inputs.add(option);
+        }
+        List<TestCase<String>> tests = new ArrayList<>();
+        for (String input : inputs) {
+            TestCase<String> testCase = new TestCase<>();
+            testCase.setAttach(input);
+            testCase.setInput(input);
+            tests.add(testCase);
+        }
+        TestCase<String> testCase = new TestCase<>();
+        testCase.setInput("rock");
+        testCase.setAttach("rock");
+        testCase.setCheckFunc(this::checkRandom);
+        tests.add(testCase);
+        return tests;
     }
+
 }
